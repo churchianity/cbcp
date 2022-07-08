fbb793574ca5b53ea870e87cbccf6f5d637648dd
churchianity <hayashi.nicholas@gmail.com>
2022-07-07 20:16:27 -0400

    win wip

--- win_platform.cpp
+++ win_platform.cpp
@@ -1,6 +1,14 @@
 
+
 // @TODO
-int copy(const char* stuff) {
+int copy(const char* stuff, int length) {
+    HGLOBAL hMem =  GlobalAlloc(GMEM_MOVEABLE, length);
+    memcpy(GlobalLock(hMem), output, length);
+    GlobalUnlock(hMem);
+    OpenClipboard(0);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
     return 0;
 }
 
